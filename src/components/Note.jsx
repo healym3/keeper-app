9bf4ac8356afcd7e46373ee1bf0953a3cedc1518
healym3 <healym3@gator.uhd.edu>
2021-11-12 21:38:29 -0600

    Update note css

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Note(){
 
-    return <div><h1>Note Title</h1>
+    return <div className="note"><h1>Note Title</h1>
     <p>This is the note.</p>
     </div>
 }
