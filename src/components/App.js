6e96151ce667f6b211e218c53602e0a00eb0f3ba
healym3 <healym3@gator.uhd.edu>
2021-11-15 17:26:30 -0600

    Update fix

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-
+//
 function App() {
   const [noteList, updateNotes] = React.useState([{title: "Note Title", content: "Note Content"}]);
 
